c66734a58eed102a50a0777b313277be56e4fcd2
k8xu <38595657+k8xu@users.noreply.github.com>
2018-12-26 13:27:41 -0500

    Create mock heroes

---
+++ my-app/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Einstein' },
+  { id: 12, name: 'Euler' },
+  { id: 13, name: 'Newton' },
+  { id: 14, name: 'Gauss' },
+  { id: 15, name: 'Planck' },
+  { id: 16, name: 'Tesla' },
+  { id: 17, name: 'Archimedes' },
+  { id: 18, name: 'Leibniz' },
+  { id: 19, name: 'Pythagoras' },
+  { id: 20, name: 'Euclid' }
+];
